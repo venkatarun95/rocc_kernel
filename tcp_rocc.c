1e37f6457d6a9db525de05098858d9b427df139b
Venkat Arun <venkatarun95@gmail.com>
2022-03-23 15:02:07 -0400

    Working without loss mode

--- tcp_rocc.c
+++ tcp_rocc.c
@@ -1,36 +1,69 @@
 /* RoCC (Robust Congestion Control
+ * For debugging use `sudo sysctl -w net.ipv4.tcp_fin_timeout=1` so you can `rmmod` the module sooner, else it will wait for 60 seconds
  */
 
 #include <linux/module.h>
+#include <linux/kernel.h>
 #include <net/tcp.h>
 #include <linux/random.h>
+
+#define ROCC_DEBUG
+
+// Should be a power of two so rocc_num_intervals_mask can be set
+static const u16 rocc_num_intervals = 16;
+// rocc_num_intervals expressed as a mask
+static const u16 rocc_num_intervals_mask = 15;
+static const u32 rocc_min_cwnd = 2;
+// Maximum tolerable loss rate, expressed as `loss_thresh / 1024`. Calculations
+// are faster if things are powers of 2
+static const u32 loss_thresh = 8;
 
 enum ROCC_MODE {
 	ROCC_REGULAR_MODE,
 	ROCC_LOSS_MODE
 };
 
+// To keep track of the number of packets acked over a short period of time
+struct rocc_interval {
+	// Starting time of this interval
+	u64 start_us;
+	u32 pkts_acked;
+	u32 pkts_lost;
+};
+
 static int id = 0;
 struct rocc_data {
+	struct rocc_interval *intervals;
+	// Index of the last interval to be added
+	u16 intervals_head;
+
+	u32 min_rtt_us;
 	enum ROCC_MODE mode;
 
 	// debug helpers
 	int id;
 };
 
-/*********************
- * Getters / Setters *
- * ******************/
-static u32 rocc_get_rtt(struct tcp_sock *tsk)
+static void rocc_init(struct sock *sk)
 {
-        /* Get initial RTT - as measured by SYN -> SYN-ACK.
-         * If information does not exist - use 1ms as a "LAN RTT".
-         */
-	if (tsk->srtt_us) {
-		return max(tsk->srtt_us >> 3, 1U);
-	} else {
-		return USEC_PER_MSEC;
+	struct rocc_data *rocc = inet_csk_ca(sk);
+	u16 i;
+
+	rocc->intervals = kzalloc(sizeof(struct rocc_interval) * rocc_num_intervals,
+				  GFP_KERNEL);
+	for (i = 0; i < rocc_num_intervals; ++i) {
+		rocc->intervals[i].start_us = 0;
+		rocc->intervals[i].pkts_acked = 0;
+		rocc->intervals[i].pkts_lost = 0;
 	}
+	rocc->intervals_head = 0;
+
+	rocc->min_rtt_us = U32_MAX;
+	rocc->mode = ROCC_REGULAR_MODE;
+	++id;
+	rocc->id = id;
+
+	cmpxchg(&sk->sk_pacing_status, SK_PACING_NONE, SK_PACING_NEEDED);
 }
 
 static u32 rocc_get_mss(struct tcp_sock *tsk)
@@ -39,44 +72,95 @@ static u32 rocc_get_mss(struct tcp_sock *tsk)
 	return tsk->mss_cache;
 }
 
-/* Initialize cwnd to support current pacing rate (but not less then 4 packets)
-vv */
-static void rocc_set_cwnd(struct sock *sk)
-{
-	struct tcp_sock *tsk = tcp_sk(sk);
-	/* u64 cwnd = sk->sk_pacing_rate; */
-
-	/* cwnd *= rocc_get_rtt(tcp_sk(sk)); */
-	/* cwnd /= tp->mss_cache; */
-	/* cwnd /= USEC_PER_SEC; */
-	/* cwnd *= 2; */
-	/* cwnd = max(4ULL, cwnd); */
-	/* cwnd = min((u32)cwnd, tp->snd_cwnd_clamp); /\* apply cap *\/ */
-
-	u32 cwnd = 4 * rocc_get_mss(tsk);
-
-	tsk->snd_cwnd = cwnd;
-}
-
-
 /* was the rocc struct fully inited */
 static bool rocc_valid(struct rocc_data *rocc)
 {
-	return rocc;
+	return (rocc && rocc->intervals);
 }
 
 static void rocc_process_sample(struct sock *sk, const struct rate_sample *rs)
 {
 	struct rocc_data *rocc = inet_csk_ca(sk);
 	struct tcp_sock *tsk = tcp_sk(sk);
-	u32 rtt = rocc_get_rtt(tsk);
+	u32 rtt_us;
+	u16 i, id;
+	u32 hist_us;
+	u64 timestamp;
+	u32 interval_length;
+	// Number of packets acked and lost in the last `hist_us`
+	u32 pkts_acked, pkts_lost;
+	u32 cwnd;
 
 	if (!rocc_valid(rocc))
 		return;
 
-	rocc_set_cwnd(sk);
+	// Is rate sample valid?
+	if (rs->delivered < 0 || rs->interval_us < 0)
+		return;
 
-	printk(KERN_INFO "rocc cwnd %u pacing %lu rtt %u mss %u", tsk->snd_cwnd, sk->sk_pacing_rate, rtt, tsk->mss_cache);
+	/* Get initial RTT - as measured by SYN -> SYN-ACK.
+         * If information does not exist - use U32_MAX as RTT
+         */
+	if (tsk->srtt_us) {
+		rtt_us = max(tsk->srtt_us >> 3, 1U);
+	} else {
+		rtt_us = U32_MAX;
+	}
+
+	if (rtt_us < rocc->min_rtt_us)
+		rocc->min_rtt_us = rtt_us;
+
+	hist_us = 2 * rocc->min_rtt_us;
+
+	// Update intervals
+	timestamp = tsk->tcp_mstamp; // Most recent send/receive
+	//timestamp = rs->prior_mstamp + rs->interval_us;
+
+	// The factor of 2 gives some headroom so that we always have
+	// sufficient history. We end up storing more history than needed, but
+	// that's ok
+	interval_length = 2 * hist_us / rocc_num_intervals + 1; // round up
+	if (rocc->intervals[rocc->intervals_head].start_us + interval_length < timestamp) {
+		// Push the buffer
+		rocc->intervals_head = (rocc->intervals_head - 1) & rocc_num_intervals_mask;
+		rocc->intervals[rocc->intervals_head].start_us = timestamp;
+		rocc->intervals[rocc->intervals_head].pkts_acked = rs->acked_sacked;
+		rocc->intervals[rocc->intervals_head].pkts_lost = rs->losses;
+	}
+	else {
+		rocc->intervals[rocc->intervals_head].pkts_acked += rs->acked_sacked;
+		rocc->intervals[rocc->intervals_head].pkts_lost += rs->losses;
+	}
+
+	// Find the statistics from the last `hist` seconds
+	pkts_acked = 0;
+	pkts_lost = 0;
+	for (i = 0; i < rocc_num_intervals; ++i) {
+		id = (rocc->intervals_head + i) & rocc_num_intervals_mask;
+		pkts_acked += rocc->intervals[id].pkts_acked;
+		pkts_lost += rocc->intervals[id].pkts_lost;
+		if (rocc->intervals[id].start_us + hist_us < timestamp) {
+			break;
+		}
+	}
+
+	// If the loss rate was too high, reduce the cwnd
+	if (pkts_lost > (pkts_acked + pkts_lost)
+
+	cwnd = pkts_acked;
+	if (cwnd < rocc_min_cwnd) {
+		cwnd = rocc_min_cwnd;
+	}
+	tsk->snd_cwnd = cwnd;
+
+#ifdef ROCC_DEBUG
+	printk(KERN_INFO "rocc cwnd %u pacing %lu rtt %u mss %u timestamp %llu interval %ld", tsk->snd_cwnd, sk->sk_pacing_rate, rtt_us, tsk->mss_cache, timestamp, rs->interval_us);
+	printk(KERN_INFO "rocc pkts_acked %u hist_us %u", pkts_acked, hist_us);
+	for (i = 0; i < rocc_num_intervals; ++i) {
+		id = (rocc->intervals_head + i) & rocc_num_intervals_mask;
+		printk(KERN_INFO "rocc intervals %llu acked %u lost %u i %u id %u", rocc->intervals[id].start_us, rocc->intervals[id].pkts_acked, rocc->intervals[id].pkts_lost, i, id);
+	}
+#endif
 
 	if (rocc->mode == ROCC_LOSS_MODE)
 		goto end;
@@ -84,17 +168,6 @@ static void rocc_process_sample(struct sock *sk, const struct rate_sample *rs)
  end:;
 	//pcc->lost_base = tsk->lost;
 	//pcc->delivered_base = tsk->delivered;
-}
-
-static void rocc_init(struct sock *sk)
-{
-	struct rocc_data *rocc = inet_csk_ca(sk);
-
-	rocc->mode = ROCC_REGULAR_MODE;
-	++id;
-	rocc->id = id;
-
-	cmpxchg(&sk->sk_pacing_status, SK_PACING_NONE, SK_PACING_NEEDED);
 }
 
 static void rocc_release(struct sock *sk)
@@ -177,12 +250,12 @@ static struct tcp_congestion_ops tcp_rocc_cong_ops __read_mostly = {
 	/* Keep the windows static */
 	.undo_cwnd = rocc_undo_cwnd,
 	/* Slow start threshold will not exist */
-	.ssthresh = rocc_ssthresh,
-	.set_state	= rocc_set_state,
+	 .ssthresh = rocc_ssthresh,
+	//.set_state	= rocc_set_state,
 	.cong_avoid = rocc_cong_avoid,
-	.pkts_acked = rocc_pkts_acked,
-	.in_ack_event= rocc_ack_event,
-	.cwnd_event	= rocc_cwnd_event,
+	//.pkts_acked = rocc_pkts_acked,
+	//.in_ack_event= rocc_ack_event,
+	//.cwnd_event	= rocc_cwnd_event,
 };
 
 /* Kernel module section */
